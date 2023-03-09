db484f33ae02dca5a4663b07f58c0213ef28a5c6
harleydi <disneyharley26@gmail.com>
2023-03-09 12:48:37 -0500

    completed assignment

---
+++ main.js
@@ -0,0 +1,20 @@
+let paragraph = document.querySelector('p')
+let heading = document.querySelector('h1')
+let thirteen = document.querySelector('#item-13')
+let three = document.querySelector('#item-3')
+let image = document.querySelector('img')
+let image2 = document.querySelector('#img2')
+let sixteen = document.querySelector('#item-16')
+
+paragraph.style.color = 'lightblue'
+heading.style.fontSize = '48px'
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+thirteen.style.opacity = '0.5'
+three.innerText = 'Hi!'
+image.src = 'https://images.pexels.com/photos/15710113/pexels-photo-15710113.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load'
+
+image.style.height = '300px'
+image2.style.height = '300px'
+sixteen.className = 'item'
+sixteen.innerText = "Won't get fooled again."
